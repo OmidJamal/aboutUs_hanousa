fdf7f37e603cb83c1a3bee71074dc2366b0a6d53
OmidJamal <omidjamal.oji@gmail.com>
2023-04-08 00:01:51 +0430

    update Files

---
+++ assets/js/aboutUs.js
@@ -0,0 +1,62 @@
+var swiper = new Swiper(".consSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300:{
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".custSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 70,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+});
